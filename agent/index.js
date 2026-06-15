56a6b57b241a394f4e6814ca6e66da8d79c3fe6a
Rudra <rudra.agrawal321@gmail.com>
2026-06-15 11:13:26 +0000

    feat(agent): agent initialize

---
+++ agent/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const ws = require('ws');
+const port = 3000
+
+const server = http.createServer(app)
+const WebSocketServer = ws.WebSocketServer
+const wss = new WebSocketServer({server})
+
+wss.on('connection', (ws)=>{
+    console.log('client connected')
+})
+
+app.get('/api/health', (req, res) => {
+  res.json({status: "ok"})
+});
+
+server.listen(port, ()=> {
+    console.log(`Agent is running on port: ${port}`)
+})
