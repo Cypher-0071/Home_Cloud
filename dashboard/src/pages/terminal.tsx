6a1bb365dc449714d42d3d6257ce9de791ee14d7
Rudra <rudra.agrawal321@gmail.com>
2026-06-21 17:09:02 +0530

    feat(dashboard): integrate WebSocket for real-time terminal communication

--- dashboard/src/pages/terminal.tsx
+++ dashboard/src/pages/terminal.tsx
@@ -5,16 +5,29 @@ import '@xterm/xterm/css/xterm.css'
 
 export default function TerminalComponent(){
     const terminalRef = useRef<HTMLDivElement>(null)
+    const socketRef = useRef<WebSocket | null>(null)
 
     useEffect(()=>{
         const terminal = new Terminal()
         const fitaddon = new FitAddon()
-
+        socketRef.current = new WebSocket('wss://home-cloud.live/terminal') 
         terminal.loadAddon(fitaddon)
         terminal.open(terminalRef.current!)
         fitaddon.fit()
+
+        socketRef.current.onmessage = (event) =>{
+            terminal.write(event.data)
+        }
+
+        terminal.onData((data)=>{
+            socketRef.current?.send(data)
+        })
         
-        return () =>  terminal.dispose()
+        
+        return () =>  {
+            terminal.dispose()
+            socketRef.current?.close()
+        }
     },[])
 
     return <div ref={terminalRef} style={{ height: '100%', width: '100%' }} />
