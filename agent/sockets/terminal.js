8f0d356f8afcea6cd894e381f4ed9b0da3754960
Rudra <rudra.agrawal321@gmail.com>
2026-06-22 02:52:03 +0530

    feat(agent): update terminal setup with JWT authentication

--- agent/sockets/terminal.js
+++ agent/sockets/terminal.js
@@ -1,46 +1,62 @@
-const pty = require('node-pty')
-const os = require('os')
-const path = require('path')
+const pty = require("node-pty");
+const os = require("os");
+const path = require("path");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
 
-class PTY{
-    constructor(ws){
-        this.ws = ws;
-        this.shell = os.platform() == 'win32'?'powershell.exe':'bash';
-        
-    }
+class PTY {
+	constructor(ws) {
+		this.ws = ws;
+		this.shell = os.platform() == "win32" ? "powershell.exe" : "bash";
+	}
 
-    createPTY(){
-        this.terminal = pty.spawn(this.shell, [], {
-            name: 'xterm-256color',
-            cols: 100,
-            cwd : process.env.HOME,
-            env :  process.env
-        } )
-        this.terminal.onData((data)=>{
-            this.ws.send(data)
-        })
-    }
-    getTerminal(){
-      return this.terminal;
-    }
+	createPTY() {
+		this.terminal = pty.spawn(this.shell, [], {
+			name: "xterm-256color",
+			cols: 100,
+			cwd: process.env.HOME,
+			env: process.env,
+		});
+		this.terminal.onData((data) => {
+			this.ws.send(data);
+		});
+	}
+	getTerminal() {
+		return this.terminal;
+	}
 
-    writeTerminal(data){
-        if(this.terminal){
-            this.terminal.write(data);
-        }
-    }
+	writeTerminal(data) {
+		if (this.terminal) {
+			this.terminal.write(data);
+		}
+	}
 }
 
-function setupTerminal(wss){
-    wss.on('connection', (ws) =>{
-        const ptyinstance = new PTY(ws)
-        ptyinstance.createPTY()
-        ws.on('message', (data)=>{
-            ptyinstance.writeTerminal(data)
-        })
-    })
+function setupTerminal(wss) {
+	wss.on("connection", (ws, request) => {
+		const cookies = cookie.parse(request.headers.cookie || '')
+        const token = cookies.token
+		if (token) {
+			jwt.verify(token, process.env.JWT_SECRET, (err, decodedtoken) => {
+				if (err) {
+					ws.close();
+					return;
+				} else {
+					const ptyinstance = new PTY(ws);
+					ptyinstance.createPTY();
+					ws.on("message", (data) => {
+						ptyinstance.writeTerminal(data);
+					});
+				}
+			});
+		} else {
+			ws.close();
+			return;
+		}
+	});
 }
+
 
 module.exports = {
-    setupTerminal
-}   
+	setupTerminal,
+};
